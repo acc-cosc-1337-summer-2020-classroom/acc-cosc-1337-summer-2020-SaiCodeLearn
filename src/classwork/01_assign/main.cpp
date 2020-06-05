5269717f2a2dc5eb44a35ec447750c51481390aa
SaiCodeLearn <sairamana.sivakumar@g.austinc.edu>
2020-06-04 21:00:48 -0500

    Assignment 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,23 @@
 //Write the include statement for types.h here
 
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include "types.h"
+#include<iostream>
 
 
 int main() 
 {
+	auto num = 0;
+	std::cout<<"Enter number: ";
+	std::cin>>num;
 
+	int result = multiply_numbers(num);
+
+	std::cout<<"Result is:"<<result<<"\n";
+
+	auto num1 = 4;
+	result = multiply_numbers(num1);
+	std::cout<<"Result is:"<<result;
 	return 0;
 }
 
