1250d8977237908555f81974b4a3c1b90033d74e
SaiCodeLearn <sairamana.sivakumar@g.austinc.edu>
2020-07-16 22:52:36 -0500

    Temperature Class exercises

--- src/examples/08_module/temperature_data.cpp
+++ src/examples/08_module/temperature_data.cpp
@@ -1,1 +1,39 @@
 //temperature_data.cpp
+#include "temperature_data.h"
+
+void TemperatureData::save_temps(std::vector<Temperature>& ts)//list of temperatures
+{
+    std::ofstream file_out(file_name, std::ios_base::trunc);//ofstream to open, use truncate mode
+
+    for(auto temp: ts)
+    {
+        file_out<<temp.get_hour();
+        file_out<<" ";
+        file_out<<temp.get_reading();
+        file_out<<"\n";
+    }
+
+    file_out.close();//Explicitly closes file, if we forget, C++ still closes file for us
+}
+//^Created a file with records - We are writing to file
+
+std::vector<Temperature> TemperatureData::get_temps()const //Reading a file
+{
+    std::vector<Temperature> temps;//Create a Temperature vector
+    std::ifstream read_file(file_name);
+
+    int hour;
+    double reading;
+
+    if(read_file.is_open())//If open for reading
+    {
+        while(read_file>>hour>>reading)//Take hour and temp
+        {
+            Temperature temp(hour, reading);//Add temp and hour to vector
+            temps.push_back(temp);
+        }
+    }
+
+    read_file.close();
+    return temps;
+}
--- src/examples/08_module/temperature_data.h
+++ src/examples/08_module/temperature_data.h
@@ -1,1 +1,19 @@
 //temperature_data.h
+#ifndef TEMPERATURE_DATA_H
+#define TEMPERATURE_DATA_H
+
+#include "temperature.h"
+#include<fstream>//Allows us to use files - C++ manages file closing opening
+#include<vector>
+#include<string>
+
+class TemperatureData
+{
+public:
+    void save_temps(std::vector<Temperature>& ts);//saves a list of temps to file
+    std::vector<Temperature> get_temps()const;//reads the list from files creates temp and adds to a vector
+private:
+    const std::string file_name{"temperature.dat"};
+};
+
+#endif
