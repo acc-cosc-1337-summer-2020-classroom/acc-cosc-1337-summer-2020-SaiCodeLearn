74f838f9b4226c285b6637cf5746c585ebade744
SaiCodeLearn <sairamana.sivakumar@g.austinc.edu>
2020-07-20 23:12:30 -0500

    Basic Algorithms Review - Add to this later

--- src/examples/10_module/02_arrays_access/arrays_access.h
+++ src/examples/10_module/02_arrays_access/arrays_access.h
@@ -1,1 +1,13 @@
 //header
+#include <ctime>//Converts timer value to readable string
+#include <cstdlib>//for algorithms
+#include <chrono>//Helps maintain time units
+#include <iostream>
+
+void populate_array(int list[], int size, bool randomize);
+
+int linear_search(const int list[], int size, int search_value); //O(n)
+
+int binary_search(const int list[], int size, int search_value); //O(log 2 base n)
+
+void bubble_sort(int list[], int size); //O(n^2)
